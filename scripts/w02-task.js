309531cc201aeb6bfc3e0cc15d47257e79ad161d
Takaedza <tmandie@live.com>
2023-09-22 04:19:32 +0200

    task2in

---
+++ scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Takaedza Godfrey Mandinyenya";
+const nameElement = document.getElementById("name").innerHTML = fullName;
+
+let currentYear = new Date().getFullYear();
+const yearElement = document.querySelector("#year").textContent = currentYear;
+
+/*let profilePicture = "images/image.png";
+const imageElement = document.getElementById("home").innerHTML="<picture><img src = " + profilePicture" alt='Takaedza' class='img-responsive'/></picture>"; */
+
+const foods = ["Rice and chicken", "Sadza", "Macaroni and mincemeat", "Cottage pie", "Spanich pie"]
+const foodElement = document.getElementById("food").innerHTML = foods
+
+const favFood = ["steak"]
+foods.push(favFood)
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
