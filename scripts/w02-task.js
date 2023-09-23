2b378a3566072bef43d88e606e4f2213824c9d88
Takaedza <tmandie@live.com>
2023-09-23 15:45:54 +0200

    Task2end

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,37 +3,42 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+
 let fullName = "Takaedza Godfrey Mandinyenya";
-const nameElement = document.getElementById("name").innerHTML = fullName;
-
 let currentYear = new Date().getFullYear();
-const yearElement = document.querySelector("#year").textContent = currentYear;
+let profilePicture = "images/image.png";
 
-/*let profilePicture = "images/image.png";
-const imageElement = document.getElementById("home").innerHTML="<picture><img src = " + profilePicture" alt='Takaedza' class='img-responsive'/></picture>"; */
-
-const foods = ["Rice and chicken", "Sadza", "Macaroni and mincemeat", "Cottage pie", "Spanich pie"]
-const foodElement = document.getElementById("food").innerHTML = foods
-
-const favFood = ["steak"]
-foods.push(favFood)
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const yearElement = document.querySelector("#year");
+const foodElement = document.getElementById("food");
+const imageElement = document.querySelector('#home picture img');
 
 /* Step 4 - Adding Content */
 
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile picture of ${fullName}`);
 
 
 
 
 /* Step 5 - Array */
+const favFoods = ["Rice and chicken", "Sadza", "Macaroni and mincemeat", "Cottage pie", "Spinach pie"];
 
+foodElement.textContent = favFoods.join(",");
 
+const singleFavFood = "Cheese cake";
 
+favFoods.push(singleFavFood);
 
+foodElement.textContent = favFoods.join(",");
 
+favFoods.shift();
 
+foodElement.innerHTML += "<br>" + favFoods.join("<br>");
+
+favFoods.pop();
+
+foodElement.innerHTML += "<br>" + favFoods.join("<br>");
